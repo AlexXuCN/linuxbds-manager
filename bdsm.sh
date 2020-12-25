5ae107070f0a10e0e59de7cba2d7e51853082a04
AlexXuCN <48176762+AlexXuCN@users.noreply.github.com>
2020-12-26 01:45:28 +0800

    Update bdsm.sh

--- bdsm.sh
+++ bdsm.sh
@@ -8,20 +8,35 @@ echo 'By AlexXuCN'
 
 # function
 
-cnsetup(){
-
-}
-
-ensetup(){
-
-}
-
-# language
-
-read -p "Choose your language:(en/cn)" LANG
-if [ $LANG == 'cn' ]
+readconf(){
+if test -e ~/.bdsm/bdsm.conf
 then
-cnsetup
-elif [ $LANG == 'en' ]
-ensetup
+    source ~/.bdsm/bdsm.conf
+else
+    touch ~/.bdsm/bdsm.conf
+    echo 'BDSDIR=0' >> ~/.bdsm/bdsm.conf
+    echo 'LANG=0' >> ~/.bdsm/bdsm.conf
+    read -p "Choose your language:(CN/EN,default=EN)" TMP
+fi
+
+if [ $LANG == '0' ]
+then
+    if [ $TMP == 'EN' ]
+        LANG="EN"
+    elif [ $TMP == 'CN' ]
+        LANG="CN"
+    else
+        LANG="EN"
+    fi
+fi
+
+if [ $BDSDIR == 0 ]
+then
+    read -p "Where do you want to install BDS?(default=~/bds/)" TMP
+    if [ $TMP == '' ]
+    then
+        BDSDIR=~/bds/
+    else
+        BDSDIR=$TMP
+    fi
 fi
